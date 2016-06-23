563d73821aae475d8f949f410c4ccba16810883d
Billy Neuson <bneuson@gmail.com>
2016-06-23 17:03:55 -0400

    Added VR images, scroll presentation

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,34 +1,79 @@
-   $(document).ready(function(){
-      $("head").append($("<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css' type='text/css' media='screen' />"));
-      $("head").append($("<link rel='stylesheet' href='css/min/leaflet.min.css' type='text/css' media='screen' />"));
+function handleScroll(e)
+{
+   if(handleScroll.active)
+   {
+      delta = e.originalEvent.deltaY;
+      if (delta > 10)
+      {
+         delta = 10;
+      }
+      handleScroll.position += delta;
+      if(handleScroll.position < 0)
+      {
+         handleScroll.position = 0;
+      }
+   }
 
-      $('body').scrollspy({target: ".navbar", offset: 50});   
+   if($('#intro').position().top == 0)
+   {
+      if (handleScroll.active == false && handleScroll.position > 580)
+      {
+         handleScroll.position = 580;
+      }
+      handleScroll.active = true;
+   }
 
-      // Add smooth scrolling on all links inside the navbar
-      $("#myNavbar a").on('click', function(event) {
+   if (handleScroll.position < 581)
+   {
+      //Animate image
+      $('#modelImg').attr('src','./vr/0_'+Math.floor(handleScroll.position/20)+'.png');
+      e.preventDefault();
+      e.stopPropagation();
+   }
+   else 
+   {
+      handleScroll.active = false;
+   }
 
-         // Prevent default anchor click behavior
-         event.preventDefault();
+}
+$(document).ready(function (){
+   $("head").append($("<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css' type='text/css' media='screen' />"));
+   $("head").append($("<link rel='stylesheet' href='css/min/leaflet.min.css' type='text/css' media='screen' />"));
 
-         // Store hash
-         var hash = this.hash;
-         if (hash != "") {
-            $(".navbar-collapse.in").collapse('hide');
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({scrollTop: $(hash).offset().top}, 800, function(){
+   $('body').scrollspy({target: ".navbar", offset: 50});   
 
-               // Add hash (#) to URL when done scrolling (default click behavior)
-               window.location.hash = hash;
-            });
-         }
-      });
-     /* $(window).scroll(function() {
-           var theta = ($(window).scrollTop() * 0.25) % (2* Math.PI);
-             $('#modelImg').css({ transform: 'rotate('+window.pageYOffset+'deg)' });
-             console.log(window.pageYOffset);
-      });*/
+   // Add smooth scrolling on all links inside the navbar
+   $("#myNavbar a").on('click', function(event) {
 
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+      if (hash != "") {
+         $(".navbar-collapse.in").collapse('hide');
+         // Using jQuery's animate() method to add smooth page scroll
+         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+         $('html, body').animate({scrollTop: $(hash).offset().top}, 800, function(){
+
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+         });
+      }
    });
+   $('body').on({
+      'mousewheel':function(e){
+         handleScroll(e);
+      }
+   });
+   if($('#intro').position().top == 0){
+      handleScroll.active = true;
+      handleScroll.position = 0;
+   }
+   else{
+      handleScroll.active = false;
+   }
+
+});
 
 
